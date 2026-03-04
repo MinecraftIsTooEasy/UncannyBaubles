4659efdd4ac0cf66763e7200f99e3970a4c29b6e
Inf1nity <1449896523@qq.com>
2026-03-04 13:30:00 +0800

    Fix itfrb compat

--- src/main/java/com/inf1nlty/uncannybaubles/compat/ModCompat.java
+++ src/main/java/com/inf1nlty/uncannybaubles/compat/ModCompat.java
@@ -2,7 +2,7 @@ package com.inf1nlty.uncannybaubles.compat;
 
 import net.minecraft.EntityPlayer;
 import net.minecraft.ItemStack;
-import net.oilcake.mitelros.util.FoodDataList;
+import net.oilcake.mitelros.feat.FoodWater;
 import net.xiaoyu233.fml.FishModLoader;
 
 public class ModCompat {
@@ -10,6 +10,6 @@ public class ModCompat {
     public static final boolean HAS_ITFRB = FishModLoader.hasMod("mite-itf-reborn");
 
     public static void callITFRBFoodDataOnEaten(ItemStack stack, EntityPlayer player) {
-        FoodDataList.onFoodEaten(stack, player);
+        FoodWater.onFoodEaten(stack, player);
     }
 }
